b6b7eac68fc809d4a78acf6fe20e0a1d50505df3
parkroolucas <2109121+parkroolucas@users.noreply.github.com>
2021-03-14 15:55:16 +1000

    Video 11 complete

---
+++ chat-app/src/components/Root/Root.tsx
@@ -0,0 +1,12 @@
+import { Button, Intent } from "@blueprintjs/core";
+import React from "react";
+
+const Root = () => {
+  return (
+    <div>
+      <Button intent={Intent.SUCCESS}>Click Me</Button>
+    </div>
+  );
+};
+
+export default Root;
---
+++ chat-app/src/index.tsx
@@ -0,0 +1,9 @@
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+import "normalize.css/normalize.css";
+import React from "react";
+import { render } from "react-dom";
+
+import Root from "./components/Root";
+
+render(<Root />, document.getElementById("app"));
