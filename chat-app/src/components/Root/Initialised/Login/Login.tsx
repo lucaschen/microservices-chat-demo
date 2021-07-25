72e4454dd096f311b52db83c66cdc89a1805e829
lucaschen <2109121+lucaschen@users.noreply.github.com>
2021-07-25 17:00:23 +1000

    Video 14 complete

--- chat-app/src/components/Root/Initialised/Login/Login.tsx
+++ chat-app/src/components/Root/Initialised/Login/Login.tsx
@@ -1,5 +1,45 @@
+import { Card, Classes, Elevation, FormGroup, InputGroup } from "@blueprintjs/core";
+import styled from "styled-components";
+
+const Form = styled.form`
+  margin: auto;
+  width: 25rem;
+`;
+
+const Heading = styled.strong.attrs({ className: Classes.HEADING })`
+  display: block;
+  font-size: 1.5rem;
+  margin-bottom: 0.75rem;
+`;
+
+const LargeFormGroup = styled(FormGroup)`
+  .bp3-label {
+    font-size: 1rem;
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100%;
+  width: 100%;
+`;
+
 const Login = () => {
-  return <h1>Login</h1>;
+  return (
+    <Wrapper>
+      <Form>
+        <Card elevation={Elevation.TWO}>
+          <Heading>Chat App</Heading>
+          <LargeFormGroup label="Username">
+            <InputGroup autoFocus large />
+          </LargeFormGroup>
+          <LargeFormGroup label="Password">
+            <InputGroup large type="password" />
+          </LargeFormGroup>
+        </Card>
+      </Form>
+    </Wrapper>
+  );
 };
 
 export default Login;
--- chat-app/src/index.tsx
+++ chat-app/src/index.tsx
@@ -5,14 +5,28 @@ import "normalize.css/normalize.css";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import { createGlobalStyle } from "styled-components";
 
 import apolloClient from "#root/api/apolloClient";
 import Root from "#root/components/Root";
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
+
+  html, body, #app {
+    height: 100%;
+    width: 100%;
+  }
+`;
 
 render(
   <ApolloProvider client={apolloClient}>
     <RecoilRoot>
       <BrowserRouter>
+        <GlobalStyle />
         <Root />
       </BrowserRouter>
     </RecoilRoot>
